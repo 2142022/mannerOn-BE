471b582eeeeabfd631ca7b4ed3bd429bd0c1a713
214 <nalee9801@gmail.com>
2023-12-15 03:37:55 +0900

    FEAT : 채팅방 생성 구현

--- src/main/java/com/manneron/manneron/chat/controller/ChatController.java
+++ src/main/java/com/manneron/manneron/chat/controller/ChatController.java
@@ -4,16 +4,20 @@ import com.manneron.manneron.chat.dto.AnswerResDto;
 import com.manneron.manneron.chat.dto.ChatResDto;
 import com.manneron.manneron.chat.dto.ChatroomResDto;
 import com.manneron.manneron.chat.dto.QuestionReqDto;
+import com.manneron.manneron.chat.entity.Chatroom;
 import com.manneron.manneron.chat.service.ChatService;
 import com.manneron.manneron.chat.service.ChatroomService;
 import com.manneron.manneron.chat.service.PromptService;
 import com.manneron.manneron.common.dto.ResDto;
+import com.manneron.manneron.common.security.UserDetailsImpl;
 import com.manneron.manneron.user.dto.UserResDto;
 import com.manneron.manneron.user.entity.User;
 import com.manneron.manneron.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,36 +30,26 @@ public class ChatController {
     private final ChatService chatService;
     private final PromptService promptService;
 
-//    @PostMapping("/prompt")
-//    @Operation(summary = "프롬프트로 채팅방 생성")
-//    public ResDto<AnswerResDto> createChatroomByPrompt(@RequestBody QuestionReqDto questionReqDto){
-//        // 회원 조회
-//        // 채팅방 생성
-//        // 프롬프트의 치환 메시지 확인
-//        // 답변 요청
-//        return ;
-//    }
-//
-//    @PostMapping("/type")
-//    @Operation(summary = "직접 메시지를 입력하여 채팅방 생성")
-//    public ResDto<AnswerResDto> createChatroomByTyping(@RequestBody QuestionReqDto questionReqDto){
-//        // 회원 조회
-//        // 채팅방 생성
-//        // 답변 요청
-//        return ;
-//    }
-//
+    @PostMapping("/start")
+    @Operation(summary = "채팅방 생성")
+    public ResDto<AnswerResDto> startChat(@RequestBody QuestionReqDto questionReqDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return chatService.startChat(questionReqDto, userDetails.user());
+
+    }
+
+
 //    @PostMapping("/{chatroom_id}")
 //    @Operation(summary = "기존 채팅방에서 답변 요청")
 //    public ResDto<AnswerResDto> getAnswer(@PathVariable Long chatroomId, @RequestBody QuestionReqDto questionReqDto) {
 //        // 회원 조회
 //        // 이전 채팅 메시지 내용 조회
+//        // 채팅 저장
 //        // 답변 요청
 //        return ;
 //    }
 //
 //    @GetMapping("/")
-//    @Operation(summary = "채팅 목록 조회")
+//    @Operation(summary = "채팅방 목록 조회")
 //    public ResDto<ChatroomResDto> getChatroomList(){
 //        // 회원 조회
 //        // 채팅방 목록 조회
@@ -69,4 +63,7 @@ public class ChatController {
 //        // 이전 채팅 내용 조회
 //        return ;
 //    }
+
+    // 좋아요 / 싫어요 클릭
+    // 복사 횟수 증가
 }
--- src/main/java/com/manneron/manneron/chat/service/ChatroomService.java
+++ src/main/java/com/manneron/manneron/chat/service/ChatroomService.java
@@ -1,6 +1,8 @@
 package com.manneron.manneron.chat.service;
 
 import com.manneron.manneron.chat.dto.QuestionReqDto;
+import com.manneron.manneron.chat.entity.Chatroom;
+import com.manneron.manneron.chat.repository.ChatroomRepository;
 import com.manneron.manneron.common.dto.ResDto;
 import com.manneron.manneron.user.entity.User;
 import lombok.RequiredArgsConstructor;
@@ -12,9 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class ChatroomService {
+    private final ChatroomRepository chatroomRepository;
+
     // 채팅방 생성
-//    @Transactional
-//    public int createChatroom(User user, QuestionReqDto questionReqDto){
-//
-//    }
+    @Transactional
+    public Chatroom createChatroom(User user, QuestionReqDto questionReqDto){
+        Chatroom chatroom = new Chatroom(user, questionReqDto.getContent(), questionReqDto.getCategory());
+        return chatroomRepository.save(chatroom);
+    }
 }
