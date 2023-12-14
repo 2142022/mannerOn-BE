707b588cf4bad74c1709b8d9ac4f4a0693caf7ab
214 <nalee9801@gmail.com>
2023-12-15 03:35:58 +0900

    FEAT : 채팅 관련 DTO 수정

--- src/main/java/com/manneron/manneron/chat/dto/ChatroomResDto.java
+++ src/main/java/com/manneron/manneron/chat/dto/ChatroomResDto.java
@@ -1,4 +1,22 @@
 package com.manneron.manneron.chat.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
 public class ChatroomResDto {
+    @Schema(description = "채팅방 고유 번호")
+    private final Long chatroomId;
+
+    @Schema(description = "채팅방 제목")
+    private final String title;
+
+    @Schema(description = "채팅방 카테고리")
+    private final String category;
+
+    public ChatroomResDto(Long chatroomId, String title, String category) {
+        this.chatroomId = chatroomId;
+        this.title = title;
+        this.category = category;
+    }
 }
