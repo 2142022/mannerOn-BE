c6174f3a167649853c17c5d525bc1bceb75c5eca
park zihoon <bbakzi417@gmail.com>
2023-12-15 14:21:58 +0900

    REFACTOR : User 닉네임 관련 유효성 검사및 공란 허용 여부 변경

--- src/main/java/com/manneron/manneron/user/repository/UserRepository.java
+++ src/main/java/com/manneron/manneron/user/repository/UserRepository.java
@@ -9,8 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByNickname(String nickName);
-
     Optional<User> findByEmail(String email);
 
     Boolean existsByEmail(String email);
